67d015389c479453361c4c7b99b6127feef99e52
kehdcode <kehindedawood@gmail.com>
2019-01-28 23:20:26 +0100

    navbar component done

---
+++ github-finder/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+       <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+       <span className="navbar-brand mb-0 mx-auto text-center"><i className="fab fa-github"></i> GITHUB FINDER</span>
+       </nav>
+      </div>
+    )
+  }
+}
+export default Navbar
